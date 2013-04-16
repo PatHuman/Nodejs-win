1fb471ed44ebbae53ba2e175e3e7dd581550603b
PatHuman <bajeneza@gmail.com>
2013-04-16 16:53:05 +0200

    initial drop

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+	var template_engine = req.app.settings.template_engine;
+	res.locals.session = req.session;
+  res.render('index', { title: 'Express with '+template_engine });
+};
+
+exports.vid = function(req, res){
+	var template_engine = req.app.settings.template_engine;
+	res.locals.session = req.session;
+  res.render('index', { title: 'Express with '+template_engine });
+};
